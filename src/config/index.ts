4ee5377fb07c9b5a36787a83404e1abb4242c7d1
TowhidAust <007towhid2016@gmail.com>
2022-10-02 00:50:55 +0600

    cleanup unnecessary codes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
 export { default as theme } from './theme';
 export { default as config } from './app';
-export { default as userRoles } from './roles';
--- src/config/roles/index.ts
+++
@@ -1,8 +0,0 @@
-const userRoles = {
-	DOCTOR: 'DOCTOR',
-	DOCTOR_ASSISTANT: 'DOCTOR_ASSISTANT',
-	DOCTOR_ADMIN_STAFF: 'DOCTOR_ADMIN_STAFF',
-	CLINIC_ADMIN: 'CLINIC_ADMIN',
-};
-
-export default userRoles;
